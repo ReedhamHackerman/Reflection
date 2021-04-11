a91d45ec30741e3d4099ec595f1d6acd1e991e93
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-04-11 01:41:42 -0400

    Initial Commit 2

---
+++ Assets/Scripts/Types.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Types : MonoBehaviour
+{
+    string stringAwake;
+    void Start()
+    {
+        System.Type type = Type.GetType(stringAwake);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
